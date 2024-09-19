316671a622fdc01dcef7b8547001f20e4b6e3c1f
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 15:52:55 +0900

    implement Create (CRUD)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
@@ -27,11 +27,23 @@ const mockTodo = [
 
 function App() {
   const [todo, setTodo] = useState(mockTodo);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
 
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   );
--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    onCreate(content);
+  };
+
   return (
     <div className="TodoEditor">
       <h4>Add new To-do</h4>
       <div className="editor_wrapper">
-        <input />
-        <button>Add</button>
+        <input value={content} onChange={onChangeContent} />
+        <button onClick={onSubmit}>Add</button>
       </div>
     </div>
   );
