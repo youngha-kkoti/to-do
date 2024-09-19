772fc6cd4d9483633211d4f02a097c2574119a34
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 15:08:34 +0900

    make TodoList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TodoEditor />
-      <div>To-do List</div>
+      <TodoList />
     </div>
   );
 }
---
+++ src/component/TodoList.js
@@ -0,0 +1,12 @@
+import "./TodoList.css";
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <h4>To-do List</h4>
+      <input className="searchbar" />
+    </div>
+  );
+};
+
+export default TodoList;
